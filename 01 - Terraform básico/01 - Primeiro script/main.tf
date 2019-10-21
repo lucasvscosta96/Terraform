805da87a34aaaf8d81f3ee6126cf03c73dac2747
Lucas Costa <lucas.vscosta@gmail.com>
2019-10-21 14:47:27 -0300

    Create Bucket

--- 01 - Terraform básico/01 - Primeiro script/main.tf
+++ 01 - Terraform básico/01 - Primeiro script/main.tf
@@ -5,3 +5,12 @@ provider "aws"{
     secret_key = "vQYBqqDpy3H51WEUlwOX/vxAn+MiS5Svh4o1wOEn"
     region     = "us-east-1"
 }
+
+resource "aws_s3_bucket" "b" {
+    bucket = "first_bucket_terraform"
+    acl    = "private"
+    tags{
+        Name = "MY bucket" 
+        Environment = "Dev"
+    }
+}
